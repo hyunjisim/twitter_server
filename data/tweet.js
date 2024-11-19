bca5ab665573af4f199fce54e3705a298bd6ada8
심현지 <tlaguswl3461@gmail.com>
2024-11-19 11:52:23 +0900

    mongoose 적용

--- data/tweet.js
+++ data/tweet.js
@@ -1,59 +1,46 @@
-import MongoDb from 'mongodb'
-import { getTweets } from '../db/database.js'
+import Mongoose from 'mongoose'
+import { useVirtualId } from '../db/database.js'
 import * as UserRepository from './auth.js'
 // 번호를 넣지 않고 트윗에 들어갈 유저가 있다면 통째로 객체로 넣어버림
-const ObjectID = MongoDb.ObjectId
+
+const tweetSchema = new Mongoose.Schema({
+    text: {type: String, require: true},
+    userId: {type: String, require: true},
+    name: {type: String, require: true},
+    username: {type: String, require: true},
+    url: String
+},{timestamps: true})
+
+useVirtualId(tweetSchema)
+const Tweet = Mongoose.model('Tweet',tweetSchema)
 
 export async function getAll() {
-    return getTweets().find().sort({ createdAt: -1 }).toArray().then(mapTweets)
+    return Tweet.find().sort({createdAt: -1})
+}
+
+export async function getAllByUsername(username) {
+    return Tweet.find({username}).sort({createdAt: -1})
+}
+
+export async function getById(id) {
+    return Tweet.findById(id)
 }
 
 export async function create(text,userId) {
     return UserRepository.findById(userId)
-        .then((user)=> getTweets().insertOne({
+        .then((user)=> new Tweet({
             text,
-            createdAt: new Date(),
             userId,
             name: user.name,
-            username: user.username,
-            url: user.url
-        }))
-        .then((result)=> {
-            return getTweets().findOne({ _id: result.insertedId})
-        })
-        .then(mapOptionalTweet)
-}
-
-export async function getAllByUsername(username) {
-    return getTweets()
-        .find({username})
-        .sort({ createdAt: -1 }) // 최신순 정렬
-        .toArray() // 배열로 변환
-        .then(mapTweets)
-}
-
-export async function getById(id) {
-    return getTweets()
-        .find({_id: new ObjectID(id)}) // ID로 단일 문서 검색
-        .next()
-        .then(mapOptionalTweet)
+            username: user.username
+        }).save())
 }
 
  // ID로 단일 문서 검색
 export async function update(id, text) {
-    return getTweets().findOneAndUpdate(
-            { _id: new ObjectID(id)},{ $set: {text} },{ returnDocument: 'after' }
-        ).then((result) => result).then(mapOptionalTweet)
+    return Tweet.findByIdAndUpdate(id, {text}, {returnDocument:'after'})
 }
 
 export async function remove(id) {
-    return getTweets().deleteOne({ _id: new ObjectID(id)})
-}
-
-function mapOptionalTweet(tweet) {
-    return tweet ? { ...tweet, id: tweet._id.toString() } : tweet
-}
-
-function mapTweets(tweets) {
-    return tweets.map(mapOptionalTweet)
+    return Tweet.findByIdAndDelete(id)
 }
